6ed756396f302101150bd618ad358a6f6f3f51f1
Nelson Filipe Rodrigues Nunes <nfrn@hotmail.com>
2018-10-22 20:09:38 +0100

    Search Basics

--- main.py
+++ main.py
@@ -1,93 +1,94 @@
-import nltk
-from collections import Counter
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
-
-def quicksort(array, low, high):
-    if (low < high) :
-        pivot_location = partition(array,low,high)
-        quicksort(array, low, pivot_location)
-        quicksort(array, pivot_location + 1, high)
-
-def partition(array,low,high):
-    pivot = array[low]
-    leftwall = low
-    for i in range(low+1,high+1):
-        if array[i] < pivot :
-            leftwall += 1
-            array[leftwall],  array[i] = array[i], array[leftwall]
-
-    array[leftwall], array[low] = array[low], array[leftwall]
-
-    return leftwall
-
-def readarraynumeric(path):
-    f = open(path, encoding="utf8")
-    array = []
-    for line in f:
-        array.append(int(line.split()[0]))
-    f.close()
-
-    print(array)
-    quicksort(array,0,len(array)-1)
-    print(array)
-
-def readarraytext(path):
-    f = open(path, encoding="utf8")
-    thisdict = {}
-    for line in f:
-        line = line.split()
-        for word in line:
-            if word in thisdict.keys():
-                thisdict[word] += 1
-            else:
-                thisdict[word] = 1
-    f.close()
-    return thisdict
-
-def readarraytextcompare(path1, path2):
-    dic1 =readarraytext(path1)
-    dic2 = readarraytext(path2)
-    i = 0
-    for key in dic1.keys():
-        if key in dic2.keys():
-            i+=1
-    print(i)
+import pandas as pd
+import os
+from sklearn.model_selection import train_test_split
+import whoosh.index as index
+import whoosh.query as query
+from whoosh.fields import Schema, TEXT, STORED, BOOLEAN, NUMERIC, DATETIME,ID
+from whoosh.index import create_in, open_dir
+from whoosh.qparser import *
 
 
-def useNTLK():
-    f = open('text13.txt', encoding="utf8")
-    doc = f.read()
-    processed = nltk.pos_tag(nltk.word_tokenize(doc))
-    print(Counter([j for i, j in processed]))
 
+PATH="en_docs.csv"
+FLAG_CREATE = True
 
-def compareDocs(path1, path2):
-    f = open(path1, encoding="utf8")
-    doc1 = f.readlines()
-    f.close()
+if not os.path.exists("indexdir"):
+   os.mkdir("indexdir")
 
-    f = open(path2, encoding="utf8")
-    doc2 = f.readlines()
-    f.close()
+def readData():
+    data = pd.read_csv(PATH, encoding="utf8")
+    data.fillna("0", inplace=True)
+    return data
 
-    bow_transformer = TfidfVectorizer()
-    bow_transformer.fit(doc1,doc2)
+def createSchema(data):
+    print("Create schema")
+    schema = Schema(number=NUMERIC,
+                    text=TEXT,
+                    cmp_code=NUMERIC,
+                    eu_code=NUMERIC,
+                    pos=NUMERIC,
+                    manifesto_id=TEXT,
+                    party=NUMERIC,
+                    date=NUMERIC,
+                    language=TEXT,
+                    source=TEXT,
+                    has_eu_code=BOOLEAN,
+                    is_primary_doc=BOOLEAN,
+                    may_contradict_core_dataset=BOOLEAN,
+                    url_original=TEXT,
+                    md5sum_text=TEXT,
+                    md5sum_original=TEXT,
+                    annotations=BOOLEAN,
+                    handbook=NUMERIC,
+                    is_copy_of=ID,
+                    title=TEXT,
+                    id=TEXT)
 
-    data1 = bow_transformer.transform(doc1)
-    data2 = bow_transformer.transform(doc2)
+    ix = create_in("indexdir", schema)
+    writer = ix.writer()
+    print("Write in schema")
+    for index, row in data.iterrows():
+        print(index)
+        writer.add_document(number=row[0],
+                            text=row[1],
+                            cmp_code=row[2],
+                            eu_code=row[3],
+                            pos=row[4],
+                            manifesto_id=row[5],
+                            party=row[6],
+                            date=row[7],
+                            language=row[8],
+                            source=row[9],
+                            has_eu_code=row[10],
+                            is_primary_doc=row[11],
+                            may_contradict_core_dataset=row[12],
+                            url_original=row[13],
+                            md5sum_text=row[14],
+                            md5sum_original=row[15],
+                            annotations=row[16],
+                            handbook=row[17],
+                            is_copy_of=row[18],
+                            title=row[19],
+                            id=row[20])
 
-    print(data1)
-    print("_")
-    print(data2)
+    print()
+    writer.commit()
+    return ix
+def searchManifest(ix,qr):
 
-    print(cosine_similarity(data1, data2))
-
-
+    print("Search query")
+    results = ix.searcher().search(query.Every())
+    print("Unordered results")
+    print(results)
 
 if __name__ == '__main__':
-    readarraynumeric('text12.txt')
-    print(readarraytext('text13.txt'))
-    readarraytextcompare('text13.txt','text14.txt')
-    useNTLK()
-    compareDocs('text13.txt','text14.txt')
+    print("Start reading data")
+    data = readData()
+
+    if FLAG_CREATE:
+        ix = createSchema(data)
+    else:
+        ix = index.open_dir("indexdir")
+
+    searchManifest(ix,u"world")
+
