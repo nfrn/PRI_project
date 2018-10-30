48b8ac9b736fa2487d5a2a46689a9030fbc26052
Miguelreis <miguel_reis_1996@hotmail.com>
2018-10-30 03:04:42 +0000

    work on Ex1, clean data based on 'md5sum_text'

--- Ex1.py
+++ Ex1.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import os
+import sys
 from sklearn.model_selection import train_test_split
 import whoosh.index as index
 import whoosh.query as query
@@ -10,85 +11,90 @@ from whoosh.qparser import *
 
 
 PATH="en_docs.csv"
-FLAG_CREATE = True
+FLAG_CREATE = False
 
 if not os.path.exists("indexdir"):
    os.mkdir("indexdir")
 
 def readData():
+    print("Start reading data...")
     data = pd.read_csv(PATH, encoding="utf8")
     data.fillna("0", inplace=True)
-    return data
+    #data = data.groupby('id', as_index=False).agg(lambda x: x.tolist())
+    print("making cleaning csv...")
+    cleaned = data.groupby("md5sum_text", as_index=False).agg(lambda x: x.tolist())
+
+    for index, row in cleaned.iterrows():
+        realtext = ' '.join(row['text'])
+        row['text'] = realtext
+
+        #just check for potential errors, do nothing for now
+        for potential_error_column in ("title", "date", "party"):
+            if( not all(x==row[potential_error_column][0] for x in row[potential_error_column])):
+                print("not cleaned properly row:", index, "| column name:", potential_error_column)
+
+        #compensate for poor joining becaus ims stewpid
+        for column in ("party", "title"):
+            row[column] = row[column][0]
+
+    cleaned.to_csv("test.csv")
+    #print(data.columns.values)
+    #print(len(data.columns.values))
+    #print(data.head(1))
+    return cleaned
 
 def createSchema(data):
     print("Create schema")
     schema = Schema(number=NUMERIC,
-                    text=TEXT,
-                    cmp_code=NUMERIC,
-                    eu_code=NUMERIC,
-                    pos=NUMERIC,
-                    manifesto_id=TEXT,
-                    party=NUMERIC,
-                    date=NUMERIC,
-                    language=TEXT,
-                    source=TEXT,
-                    has_eu_code=BOOLEAN,
-                    is_primary_doc=BOOLEAN,
-                    may_contradict_core_dataset=BOOLEAN,
-                    url_original=TEXT,
-                    md5sum_text=TEXT,
-                    md5sum_original=TEXT,
-                    annotations=BOOLEAN,
-                    handbook=NUMERIC,
-                    is_copy_of=ID,
-                    title=TEXT,
-                    id=TEXT)
+                    text=TEXT(stored = True),
+                    party=NUMERIC(stored = True),
+                    title=TEXT(stored = True))
 
     ix = create_in("indexdir", schema)
     writer = ix.writer()
-    print("Write in schema")
+    print("Writing in schema", end='')
     for index, row in data.iterrows():
-        print(index)
-        writer.add_document(number=row[0],
-                            text=row[1],
-                            cmp_code=row[2],
-                            eu_code=row[3],
-                            pos=row[4],
-                            manifesto_id=row[5],
-                            party=row[6],
-                            date=row[7],
-                            language=row[8],
-                            source=row[9],
-                            has_eu_code=row[10],
-                            is_primary_doc=row[11],
-                            may_contradict_core_dataset=row[12],
-                            url_original=row[13],
-                            md5sum_text=row[14],
-                            md5sum_original=row[15],
-                            annotations=row[16],
-                            handbook=row[17],
-                            is_copy_of=row[18],
-                            title=row[19],
-                            id=row[20])
+        if(int(index)%500==0):
+            print(".", end='', flush=True)
 
-    print()
+        writer.add_document(number=row[1],
+                    text=row["text"],
+                    party=row["party"],
+                    title=row["title"])
+    print(" done")
+    print("commiting schema")
     writer.commit()
     return ix
-def searchManifest(ix,qr):
+def searchManifest(ix):
+    qr = input("query: ")
+    while(qr != '-exit'):
+        with ix.searcher() as searcher:
+            qp = MultifieldParser(["title", "text"], schema=ix.schema)
+            #qp = QueryParser("title", schema=ix.schema)
+            q = qp.parse(qr)
+            results = searcher.search(q)
 
-    print("Search query")
-    results = ix.searcher().search(query.Every())
-    print("Unordered results")
-    print(results)
+            print("results:", results)
+            print("nresults:", len(results.docs()))
+            print("scores:", results.top_n )
+            
+            for docnum in results.docs():
+                pass
+                #print(searcher.stored_fields(docnum)["title"])
+        qr = input("\nquery: ")
+
 
 if __name__ == '__main__':
-    print("Start reading data")
     data = readData()
+
+    if (len(sys.argv) > 1):
+        if(sys.argv[1]=="generate"):
+            FLAG_CREATE= True
 
     if FLAG_CREATE:
         ix = createSchema(data)
     else:
         ix = index.open_dir("indexdir")
 
-    searchManifest(ix,u"world")
+    searchManifest(ix)
 
