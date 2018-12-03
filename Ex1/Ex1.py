ccd52f0cf4a547d854c34620953cc51c9ebf8d52
Miguelreis <miguel_reis_1996@hotmail.com>
2018-12-03 16:54:41 +0000

    You can now search using whoosh or using lab material

--- Ex1/Ex1.py
+++ Ex1/Ex1.py
@@ -8,9 +8,19 @@ from whoosh.fields import Schema, TEXT, STORED, BOOLEAN, NUMERIC, DATETIME,ID
 from whoosh.index import create_in, open_dir
 from whoosh.qparser import *
 from collections import Counter
+import time
+from lab import search, dot_product_similarity
 
 PATH="en_docs.csv"
+CLEANED_PATH = "en_docs_clean_test.csv"
+QUERY_KW = "QUERY"
 FLAG_CREATE = False
+WHOOSH_SEARCH = False
+
+'''
+start_time = time.time()
+main()
+print("--- %s seconds ---" % (time.time() - start_time))'''
 
 if not os.path.exists("indexdir"):
    os.mkdir("indexdir")
@@ -29,14 +39,14 @@ def readData():
 
         #just check for potential errors, do nothing for now
         for potential_error_column in ("title", "date", "party"):
-            if( not all(x==row[potential_error_column][0] for x in row[potential_error_column])):
+            if not all(x==row[potential_error_column][0] for x in row[potential_error_column]):
                 print("not cleaned properly row:", index, "| column name:", potential_error_column)
 
         #compensate for poor joining becaus ims stewpid
         for column in ("party", "title"):
             row[column] = row[column][0]
 
-    cleaned.to_csv("en_docs_clean_test.csv")
+    cleaned.to_csv( CLEANED_PATH) 
     return cleaned
 
 def createSchema(data):
@@ -89,18 +99,27 @@ def result_statistics(results, searcher, qr):
         
     return party_counts, keyword_per_party_counts
             
+def whoosh_search(searcher, qr, ix):
+    qp = MultifieldParser(["title", "text"], schema=ix.schema)
+    #qp = QueryParser("title", schema=ix.schema)
+    
+    q = qp.parse(qr)
+    results = searcher.search(q, terms = True)
+
+    return qp,q,results
 
 
 def searchManifest(ix):
-    qr = input("query: ")
+    qr = input("[METHOD_WHOOSH] " + QUERY_KW + ": ")
     while(qr != '-exit'):
         with ix.searcher() as searcher:
-            qp = MultifieldParser(["title", "text"], schema=ix.schema)
-            #qp = QueryParser("title", schema=ix.schema)
-            q = qp.parse(qr)
-            results = searcher.search(q, terms = True)
-            ndocs = len(results.docs())
+            start_time = time.time()
 
+            qp,q,results = whoosh_search(searcher, qr, ix)
+            print("---search time: %s seconds ---" % (time.time() - start_time))
+
+            ndocs = len(results.docs())
+            # print stats
             if( ndocs <= 0 ):
                 print("no results found") 
             else:
@@ -112,20 +131,61 @@ def searchManifest(ix):
                 print("number of manifestos from party: ",party_counts)
                 print("keyword per party: ",word_per_party)
 
-        qr = input("\nquery: ")
 
+        qr = input("\n" + "[METHOD_WHOOSH] " + QUERY_KW + ": ")
+
+def search_lab(CLEANED_PATH):
+
+    documents , i_index = search(CLEANED_PATH)
+
+    while True:
+        qr = input("[METHOD_LAB] " + QUERY_KW + ": ")
+
+        start_time = time.time()
+        print(qr)
+        qr = nltk.word_tokenize(qr)
+        similar_docs = dot_product_similarity(i_index, qr)
+        print(similar_docs)
+        print()
+
+        print("---search time: %s seconds ---" % (time.time() - start_time))
+        #qp,q,results,ndocs = lab.search(lab_search(searcher, qr, ix)
+
+        continue
+        # print stats
+        '''
+        if( ndocs <= 0 ):
+            print("no results found") 
+        else:
+            print("top results:", results)
+            print("number of total results:", ndocs)
+            #print("scores:", results.top_n )
+            
+            party_counts, word_per_party = result_statistics(results, searcher, qr)
+            print("number of manifestos from party: ",party_counts)
+            print("keyword per party: ",word_per_party)
+        '''
+
+        qr = input("\n" + QUERY_KW + ": ")
 
 if __name__ == '__main__':
     data = readData()
 
-    if (len(sys.argv) > 1):
-        if(sys.argv[1]=="generate"):
-            FLAG_CREATE= True
+    if (len(sys.argv) > 1 and sys.argv[1]=="whoosh"):
+        WHOOSH_SEARCH = True
 
-    if FLAG_CREATE:
-        ix = createSchema(data)
-    else:
-        ix = index.open_dir("indexdir")
+    if WHOOSH_SEARCH and len(sys.argv) > 2 and sys.argv[2]=="generate":
+        FLAG_CREATE = True
 
-    searchManifest(ix)
+    if WHOOSH_SEARCH:
+        if FLAG_CREATE:
+            ix = createSchema(data)
+        else:
+            ix = index.open_dir("indexdir")
+
+    if WHOOSH_SEARCH :
+        searchManifest(ix)
+    else:#search using lab 2
+        search_lab(PATH)
+        pass
 
---
+++ Ex1/lab.py
@@ -0,0 +1,149 @@
+import sys
+from collections import OrderedDict
+from operator import itemgetter   
+import numpy as np
+import nltk
+import sklearn.feature_extraction.text as te
+import pandas as pd
+
+class Stats():
+
+    def __init__(self, documents, inv_index):
+        self.documents = documents
+        self.inv_index = inv_index
+
+        self.update()
+
+    def update(self):
+        self.n_docs = len(self.documents)
+        self.n_terms = self.calc_terms()
+        self.n_ind_terms = len(self.inv_index)
+
+    def calc_terms(self):
+        terms= 0
+        for doc in self.documents:
+            terms += len(nltk.word_tokenize(doc))
+
+        return terms
+
+    def print_doc_index(self):
+        print(self.documents)
+        print(self.inv_index)
+
+    def __str__(self):
+        string = ""
+
+        string += "num of docs > " + str(self.n_docs) + "\n"
+        string += "num of terms > "+ str(self.n_terms) + "\n"
+        string += "num of individual terms > "+ str(self.n_ind_terms) + "\n"
+
+        return string
+
+doc_stats = None
+documents = list()
+inv_index = dict()
+idf_vals = dict()
+df_vals = dict()
+min_tfs = dict()
+max_tfs = dict()
+
+def build_inverted_indexes(features, matrix, n_docs):
+    for c in range(len(features)): #each term (each column)
+        inv_index[features[c]] = dict()
+
+        tf = []
+        for l in range (len(matrix)):
+            val  = matrix[l][c]
+            tf.append(val)
+
+            if (val != 0):
+                inv_index[features[c]][l+1] = matrix[l][c]
+
+        df = len(inv_index[features[c]])
+        df_vals[features[c]] = df
+        idf_vals[features[c]] = np.log(n_docs/df)
+
+        min_tfs[features[c]] = min(tf)
+        max_tfs[features[c]] = max(tf)
+
+    return inv_index
+
+def read_documents(csv):
+    data = pd.read_csv(csv, encoding="utf8")
+
+    for index, row in data.iterrows():
+        documents.append(row['text'])
+
+    count_vect = te.CountVectorizer()
+    transformed = count_vect.fit_transform(documents)
+
+    features = count_vect.get_feature_names()
+    matrix = transformed.toarray()
+    #getting counts
+    i_index = build_inverted_indexes(features, matrix, len(documents) )
+
+    return documents, i_index
+
+def dot_product_similarity(inv_index, terms):
+    A = dict()
+
+    for term in terms:
+        if(term not in inv_index):
+            print("term \'"+ term +"\' not in vocab")
+            continue
+        i_term_list = inv_index[term]
+        idf = idf_vals[term]
+
+        for doc, tf in i_term_list.items():
+            if(doc not in A):
+                Ad = 0
+                A[doc] = Ad
+        
+            A[doc] += tf * idf
+    #order by value
+    return sorted(A.items(), key = itemgetter(1), reverse=True )
+
+def search(csv):
+    documents, i_index = read_documents(csv)
+
+    doc_stats = Stats(documents, i_index)
+    #stats.print_doc_index()
+    #print(idf_vals)
+    #print(doc_stats)
+
+    return documents, i_index
+
+    '''
+    while(True):
+        query = input("search query: ")
+        if(query == "x"):
+            break
+
+        query = nltk.word_tokenize(query)
+        similar_docs = dot_product_similarity(query)
+        print(similar_docs)
+        print()
+
+    return
+    '''
+    '''
+    for term in sys.argv[1:]:
+        print("-term \'"+term+"\'")
+
+        df_val = 0
+        min_tf = 0
+        max_tf = 0
+        idf_val = 0
+        if (term in i_index):
+            df_val = df_vals[term]
+            min_tf = min_tfs[term]
+            max_tf = max_tfs[term]
+            idf_val = idf_vals[term]
+
+        print("df >", df_val)
+        print("min tf >", min_tf)
+        print("max tf >", max_tf)
+        print("idf >", idf_val)
+        print("")
+        '''
+    #print(documents)
